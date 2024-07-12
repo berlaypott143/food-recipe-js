0232135295ced411903a2493c85d6bca00b2369f
berlaypott143 <aldrinjaylej@gmail.com>
2024-07-13 00:52:01 +0800

    added js
    appId and appKey added and card element

---
+++ index.js
@@ -0,0 +1,7 @@
+// edemam api id and key
+const AppId = "b9a12d47";
+const AppKey = "b9a5ec8c6a3751d5d515fcef821187af";
+
+//food url
+const foodURL =
+  "https://api.edamam.com/search?q=inasal&app_id=b9a12d47&app_key=b9a5ec8c6a3751d5d515fcef821187af";
